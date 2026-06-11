6eae2bf9bbd602eefc7f2a2ea4337e1decd54dbd
jugnuprjpt <jugnuprjpt@gmail.com>
2026-06-11 15:47:28 +0530

    day-5

---
+++ backend/day5-JWT/index.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+const express = require("express");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/user");
+const connectDb = require("./Config/connectDb");
+
+const app = express();
+
+app.use(express.json());
+
+connectDb();
+
+/////// Register /////////////
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User Registered",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+/////////////// Login ////////////////////
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User Not Found",
+      });
+    }
+
+    const isMatchec = await bcrypt.compare;
+    (password, user.password);
+
+    if (!isMatchec) {
+      return res.status(401).json({
+        message: "Invalid Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.json({
+      message: "Login Success",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.listen(4545, () => {
+  console.log("server Running");
+});
